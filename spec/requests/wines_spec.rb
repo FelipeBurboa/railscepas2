36c01951b553961a754d1c8d12e607dbfa26b43b
FelipeBurboa <felipeburboa1991@gmail.com>
2022-08-20 14:07:42 -0400

    Rspec for wines

--- spec/requests/wines_spec.rb
+++ spec/requests/wines_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "Wines", type: :request do
-  describe "request list of all wines" do
-  wines = Wine.all
-  expect(response).to be_successful
+RSpec.describe WinesController, type: :controller do
+
+  describe "GET #index" do
+    it "assigns all wines as @wines" do
+      wine = Wine.create!
+      get :index, params: {}
+      expect(assigns(:wines)).to eq([wine])
+    end
   end
+
+  describe "GET #new" do
+    it "assigns a new wine as @wine" do
+      get :new, params: {}
+      expect(assigns(:wine)).to be_a_new(Wine)
+    end
+  end
+
 end
